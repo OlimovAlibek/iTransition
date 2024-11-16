32ce71d89ee7d0ce0e7b53255d8ac159ec25a340
Alibek Olimov <98762357+OlimovAlibek@users.noreply.github.com>
2024-11-16 17:51:45 +0500

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const { SHA3 } = require('crypto-js'); 
 
+const email = 'alibekolimov.info@gmail.com'; 
+const directoryPath = path.join(__dirname, 'files'); 
+
+async function getFileHashes() {
+  const files = fs.readdirSync(directoryPath);
+  const hashes = [];
+
+  files.forEach(file => {
+    const filePath = path.join(directoryPath, file);
+    const fileData = fs.readFileSync(filePath); 
+    const hash = SHA3(fileData).toString();
+    hashes.push(hash);
+  });
+
+  return hashes;
+}
+
+async function main() {
+  let hashes = await getFileHashes();
+  hashes.sort().reverse(); 
+
+  const concatenatedHashes = hashes.join('') + email.toLowerCase(); 
+  const finalHash = SHA3(concatenatedHashes).toString(); 
+
+  console.log(finalHash); 
+}
+
+main();
